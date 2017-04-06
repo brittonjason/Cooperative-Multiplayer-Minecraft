6562dad8963c8993aaff601d2afa8a16bb6093af
Applied Software Design <andrewpb@vt.edu>
2017-04-06 19:53:50 -0400

    Fixed

--- server_pickle.py
+++ server_pickle.py
@@ -1,11 +1,12 @@
 import pickle
 import json
 
-
 def server_pickle(x, y, z, token):
-	data = {{"x":x, "y":y, "z":z}, {"token":token}}
+	data = {"x":x, "y":y, "z":z, "token":token}
 	return pickle.dumps(json.dumps(data))
 
 def server_unpickle(pickled_data):
-	unpickled_data = json.loads(pickle.loads(pickled_data.decode("utf-8")))
+	unpickled_data = json.loads(pickle.loads(pickled_data))
 	return [unpickled_data["player_id"], unpickled_data["x"], unpickled_data["y"], unpickled_data["z"], unpickled_data["block_type"]] 
+
+
